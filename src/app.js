e8d6f0b2b1dd9c376018f5d2fcfdc06abe2fa8e5
Bram van Andel <andelvanbram@gmail.com>
2025-12-07 15:12:31 +0100

    Remove hardcoded CORS origins for improved flexibility

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500", "*"],
+    // origin: ["http://localhost:5500", "http://127.0.0.1:5500", "*"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
